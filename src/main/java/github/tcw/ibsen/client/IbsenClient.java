29e4889efdf21ad96c17989247a5a118c7fca333
tom <tcw@users.noreply.github.com>
2022-03-06 14:04:48 +0100

    New api

--- src/main/java/github/tcw/ibsen/client/IbsenClient.java
+++ src/main/java/github/tcw/ibsen/client/IbsenClient.java
@@ -1,18 +1,19 @@
 package github.tcw.ibsen.client;
 
 import com.google.protobuf.ByteString;
+import com.google.protobuf.Empty;
 import github.com.tcw.ibsen.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class IbsenClient {
 
     private final IbsenGrpc.IbsenBlockingStub blockingStub;
-    //private final IbsenGrpc.IbsenStub asyncStub;
+    private final IbsenGrpc.IbsenStub asyncStub;
 
     public IbsenClient(String host, int port) {
         ManagedChannelBuilder<?> channelBuilder = ManagedChannelBuilder
@@ -20,22 +21,13 @@ public class IbsenClient {
                 .usePlaintext();
         ManagedChannel channel = channelBuilder.build();
         blockingStub = IbsenGrpc.newBlockingStub(channel);
-        // asyncStub = IbsenGrpc.newStub(channel); Todo: for testing streaming write
+        asyncStub = IbsenGrpc.newStub(channel);
     }
 
-    public boolean create(String topic) {
-        Topic request = Topic.newBuilder().setName(topic).build();
-        return blockingStub.create(request).getCreated();
-    }
-
-    public boolean drop(String topic) {
-        Topic request = Topic.newBuilder().setName(topic).build();
-        return blockingStub.drop(request).getDropped();
-    }
-
-    public TopicsStatus status() {
-        Empty empty = Empty.newBuilder().build();
-        return blockingStub.status(empty);
+    public List<String> topics() {
+        TopicList list = blockingStub.list(Empty.newBuilder().build());
+        return list.getTopicsList().asByteStringList().stream()
+                .map(ByteString::toStringUtf8).collect(Collectors.toList());
     }
 
     public long write(String topic, List<ByteString> entries) {
@@ -44,65 +36,20 @@ public class IbsenClient {
         return writeStatus.getWrote();
     }
 
-    public Iterator<Entry> read(String topic) {
-        return read(topic, 0, 1000);
+    public void read(String topic, StreamObserver<OutputEntries> observer) {
+        read(topic, 0, 1000, observer);
     }
 
-    public Iterator<Entry> read(String topic, long offset) {
-        return read(topic, offset, 1000);
+    public void read(String topic, long offset, StreamObserver<OutputEntries> observer) {
+        read(topic, offset, 1000, observer);
     }
 
-    public Iterator<Entry> read(String topic, long offset, int batchSize) {
+    public void read(String topic, long offset, int batchSize, StreamObserver<OutputEntries> observer) {
         ReadParams readParams = ReadParams.newBuilder().setTopic(topic)
                 .setOffset(offset)
                 .setBatchSize(batchSize)
                 .build();
 
-        return new EntryIterator<>(blockingStub.read(readParams));
-    }
-
-    public static class EntryIterator<Entry> implements Iterator<IbsenClient.Entry> {
-
-        private final Iterator<OutputEntries> iterator;
-        private Iterator<github.com.tcw.ibsen.Entry> batchIterator = Collections.emptyIterator();
-
-        EntryIterator(Iterator<OutputEntries> iterator) {
-            this.iterator = iterator;
-        }
-
-        @Override
-        public boolean hasNext() {
-            return iterator.hasNext() || batchIterator.hasNext();
-        }
-
-        @Override
-        public IbsenClient.Entry next() {
-            if (!batchIterator.hasNext()) {
-                if (iterator.hasNext()) {
-                    OutputEntries batch = iterator.next();
-                    batchIterator = batch.getEntriesList().iterator();
-                }
-            }
-            github.com.tcw.ibsen.Entry entry = batchIterator.next();
-            return new IbsenClient.Entry(entry.getOffset(),entry.getContent());
-        }
-    }
-
-    public static class Entry {
-        private final long offset;
-        private final ByteString entry;
-
-        public Entry(long offset, ByteString entry) {
-            this.offset = offset;
-            this.entry = entry;
-        }
-
-        public long getOffset() {
-            return offset;
-        }
-
-        public ByteString getEntry() {
-            return entry;
-        }
+        asyncStub.read(readParams, observer);
     }
 }
--- src/test/java/github/tcw/ibsen/client/IbsenClientTest.java
+++ src/test/java/github/tcw/ibsen/client/IbsenClientTest.java
@@ -1,24 +1,22 @@
 package github.tcw.ibsen.client;
 
-import com.google.protobuf.ByteString;
-import github.com.tcw.ibsen.TopicStatus;
-import github.com.tcw.ibsen.TopicsStatus;
+import github.com.tcw.ibsen.Entry;
+import github.com.tcw.ibsen.OutputEntries;
+import io.grpc.stub.StreamObserver;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.output.OutputFrame;
 import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.concurrent.SynchronousQueue;
 
 import static com.google.protobuf.ByteString.copyFromUtf8;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Testcontainers
 class IbsenClientTest {
@@ -32,6 +30,7 @@ class IbsenClientTest {
     GenericContainer<?> ibsenContainer = new GenericContainer<>(DockerImageName.parse("ibsen"))
             .withExposedPorts(50001)
             .withLogConsumer(outputFrame -> System.out.print(outputFrame.getUtf8String()))
+            .withEnv("IBSEN_IN_MEMORY_ONLY", "true")
             .waitingFor(new LogMessageWaitStrategy().withRegEx(".*Ibsen grpc server started.*"));
 
     @BeforeEach
@@ -43,105 +42,76 @@ class IbsenClientTest {
     }
 
     @Test
-    void createTopic() {
-        boolean isCreated = client.create("test");
-        assertTrue(isCreated);
-    }
-
-    @Test
     void writeEntriesToTopic() {
-        client.create("test");
-        long wrote = client.write("test",
+        long totalBytesWritten = client.write("test",
                 List.of(copyFromUtf8("entry1"), copyFromUtf8("entry2"), copyFromUtf8("entry3")));
-        assertEquals(3, wrote);
+        assertEquals(78, totalBytesWritten);
     }
 
     @Test
-    void readEntriesFromTopic() {
-        client.create("test");
-        long wrote = client.write("test",
+    void readEntriesFromTopic() throws InterruptedException {
+        client.write("test",
                 List.of(copyFromUtf8("entry1"), copyFromUtf8("entry2"), copyFromUtf8("entry3")));
-        assertEquals(3, wrote);
-        Iterator<IbsenClient.Entry> iterator = client.read("test", 0, 10);
-        IbsenClient.Entry firstEntry = iterator.next();
-        assertEquals(1, firstEntry.getOffset());
-        assertEquals(copyFromUtf8("entry1"), firstEntry.getEntry());
+        SynchronousQueue<List<Entry>> queue = new SynchronousQueue<>();
+        client.read("test", new StreamObserver<>() {
+            @Override
+            public void onNext(OutputEntries value) {
+                queue.offer(value.getEntriesList());
+            }
 
-        IbsenClient.Entry secondEntry = iterator.next();
-        assertEquals(2, secondEntry.getOffset());
-        assertEquals(copyFromUtf8("entry2"), secondEntry.getEntry());
+            @Override
+            public void onError(Throwable t) {
+            }
 
-        IbsenClient.Entry thirdEntry = iterator.next();
-        assertEquals(3, thirdEntry.getOffset());
-        assertEquals(copyFromUtf8("entry3"), thirdEntry.getEntry());
+            @Override
+            public void onCompleted() {
+            }
+        });
 
-        assertFalse(iterator.hasNext());
+        List<Entry> entries = queue.take();
+        assertEquals(3, entries.size());
+        assertEquals(copyFromUtf8("entry1"), entries.get(0).getContent());
+        assertEquals(copyFromUtf8("entry2"), entries.get(1).getContent());
+        assertEquals(copyFromUtf8("entry3"), entries.get(2).getContent());
     }
 
     @Test
     void statusFrom2Topics() {
-        assertTrue(client.create("test1"));
-        long wrote = client.write("test1",
+        client.write("test1",
                 List.of(copyFromUtf8("entry1"), copyFromUtf8("entry2"), copyFromUtf8("entry3")));
-        assertEquals(wrote, 3L);
-        assertTrue(client.create("test2"));
-        wrote = client.write("test2",
+        client.write("test2",
                 List.of(copyFromUtf8("entry1"), copyFromUtf8("entry2")));
-        assertEquals(wrote, 2L);
-        TopicsStatus status = client.status();
-        List<TopicStatus> topicStatusList = status.getTopicStatusList();
-        TopicStatus topicStatus = topicStatusList.get(0);
-        assertEquals(topicStatus.getBlocks(), 1);
+        List<String> topics = client.topics();
+        Assertions.assertEquals(2, topics.size());
+        Assertions.assertTrue(topics.contains("test1"));
+        Assertions.assertTrue(topics.contains("test2"));
     }
 
     @Test
-    void readEntriesFromTopicWithOffsetNot0() {
-        client.create("test");
-        long wrote = client.write("test",
+    void readEntriesFromTopicWithOffsetNot0() throws InterruptedException {
+        client.write("test",
                 List.of(copyFromUtf8("entry1"), copyFromUtf8("entry2"), copyFromUtf8("entry3")));
-        assertEquals(3, wrote);
-        Iterator<IbsenClient.Entry> iterator = client.read("test", 1, 10);
-
-        IbsenClient.Entry secondEntry = iterator.next();
-        assertEquals(2, secondEntry.getOffset());
-        assertEquals(copyFromUtf8("entry2"), secondEntry.getEntry());
-
-        IbsenClient.Entry thirdEntry = iterator.next();
-        assertEquals(3, thirdEntry.getOffset());
-        assertEquals(copyFromUtf8("entry3"), thirdEntry.getEntry());
-
-        assertFalse(iterator.hasNext());
-    }
-
-    @Test
-    void readMultipleBatchesOfEntriesFromTopic() {
-        client.create("test");
-        int entrySeq = 1;
-        for (int i = 0; i < BATCHES; i++) {
-            List<ByteString> batch = new ArrayList<>();
-            for (int j = 0; j < ENTRIES_IN_BATCH; j++) {
-                batch.add(copyFromUtf8("entry-" + entrySeq++));
+        SynchronousQueue<List<Entry>> queue = new SynchronousQueue<>();
+        client.read("test", 1, new StreamObserver<>() {
+            @Override
+            public void onNext(OutputEntries value) {
+                queue.offer(value.getEntriesList());
             }
-            client.write("test", batch);
-        }
-        Iterator<IbsenClient.Entry> iterator = client.read("test", 0, 1000);
-        for (int i = 1; i < (BATCHES * ENTRIES_IN_BATCH) + 1; i++) {
-            IbsenClient.Entry entry = iterator.next();
-            assertEquals(entry.getOffset(), i);
-            assertEquals(entry.getEntry(), copyFromUtf8("entry-" + i));
-        }
+
+            @Override
+            public void onError(Throwable t) {
+            }
+
+            @Override
+            public void onCompleted() {
+            }
+        });
+
+        List<Entry> entries = queue.take();
+        assertEquals(2, entries.size());
+        assertEquals(copyFromUtf8("entry2"), entries.get(0).getContent());
+        assertEquals(copyFromUtf8("entry3"), entries.get(1).getContent());
+
     }
 
-
-    @Test
-    void dropTopic() {
-        client.create("test");
-        long wrote = client.write("test",
-                List.of(copyFromUtf8("entry1"), copyFromUtf8("entry2"), copyFromUtf8("entry3")));
-        assertEquals(wrote, 3L);
-        Iterator<IbsenClient.Entry> iterator = client.read("test", 0, 10);
-        assertTrue(iterator.hasNext());
-        boolean isDropped = client.drop("test");
-        assertTrue(isDropped);
-    }
 }
